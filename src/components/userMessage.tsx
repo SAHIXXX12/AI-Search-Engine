97186f332274a01c71b36acd9a4ddda43b5bb957
Beadchain15 <bead21215@gmail.com>
2025-02-06 16:10:10 +0900

    fix usermessage fontsize

--- src/components/userMessage.tsx
+++ src/components/userMessage.tsx
@@ -3,7 +3,7 @@
 const UserMessage = ({text}:{text: string}) => {
     return (
         <div>
-              <div className={`inline-block w-full p-2 rounded-lg bg-[#35302a] text-[#f6f3ec]`}>
+              <div className={`inline-block w-full p-2 rounded-lg bg-[#35302a] text-[#f6f3ec] text-xl`}>
                 {text}
               </div>
         </div>
