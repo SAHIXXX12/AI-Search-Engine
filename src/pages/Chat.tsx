ca952a06c73576575da66d0564f2621f0afef931
Beadchain15 <bead21215@gmail.com>
2025-02-06 14:01:56 +0900

    fix button style

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import UserMessage from "../components/userMessage";
 import BotMessage from "../components/botMessage";
 import SourceMessage from "../components/sourceMessage";
-import { ArrowRight } from "lucide-react";
+// import { ArrowRight } from "lucide-react";
 import Thinking from "../components/thinking";
 
 interface Message {
@@ -63,7 +63,7 @@ function App() {
   }, [input, messages]);
 
   return (
-    <div className="flex flex-col h-screen w-screen bg-[#b7a58f] pt-20 text-black">
+    <div className="flex flex-col h-screen w-screen bg-transparent pt-20 text-black">
       {/* <button
         onClick={handleNewThread}
         className="absolute w-fit rounded-lg bg-gray-500 px-4 py-2 mx-4 text-white hover:bg-gray-600"
@@ -71,12 +71,11 @@ function App() {
         New Chat
       </button> */}
       {/* Chat Area */}
-      {messages.length ? (
         <div
           id="chat-container"
           className="w-screen flex flex-col items-center self-center overflow-y-auto p-4 space-y-4"
         >
-          <div className="flex-1 max-w-4xl w-full items-center self-center p-4 pb-24 space-y-4">
+          <div className="flex-1 max-w-4xl w-full items-center self-center p-4 pb-2 space-y-4">
             {messages.map((msg, index) => (
               <div key={index} className={msg.type === "user" ? "" : ""}>
                 {/* <span className={`inline-block p-2 rounded-lg ${msg.type === 'user' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black'}`}>
@@ -98,11 +97,6 @@ function App() {
             {thinking && <Thinking />}
           </div>
         </div>
-      ) : (
-        <h1 className="text-center text-5xl text-[#574c3f] pt-60">
-          What can I help you with?
-        </h1>
-      )}
 
       {/* Footer */}
       <div
@@ -111,7 +105,7 @@ function App() {
         <form onSubmit={handleSubmit} className="relative flex px-4 pr-6">
           <textarea
             disabled={thinking}
-            placeholder="Ask a follow-up question..."
+            // placeholder="Ask a follow-up question..."
             value={input}
             onChange={(e) => setInput(e.target.value)}
             onKeyDown={(e) => {
@@ -120,17 +114,18 @@ function App() {
                 handleSubmit(e);
               }
             }}
-            className="w-full p-4 pr-24 rounded-lg border border-gray-300 focus:outline-none focus:border-gray-400 bg-[#35302a] shadow-[0_0_15px_rgba(0,0,0,0.5)] shadow-white text-white"
-            rows={2}
+            className="w-full p-4 mr-32 rounded-full border border-gray-300 focus:outline-none focus:border-gray-400 bg-[#f5f2eb] 1shadow-[0_0_15px_rgba(0,0,0,0.5)] 1shadow-white text-[#574c3f]"
+            rows={1}
             style={{ resize: "none" }}
             aria-label="Chat message input"
           />
           <button
             type="submit"
-            className="absolute right-8 top-1/2 -translate-y-1/2 p-2 text-[#794444] bg-[#b7a58f] rounded-full hover:text-[#794444]"
+            className="absolute right-8 top-1/2 -translate-y-1/2 p-3.5 px-6 text-xl text-[#794444] bg-[#342f29] rounded-full hover:text-[#794444]"
             aria-label="Send message"
           >
-            <ArrowRight />
+            Search
+            {/* <ArrowRight /> */}
           </button>
         </form>
       </div>
